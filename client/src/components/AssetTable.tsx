9e0c1042176c240b52ed2766ae552e9c71e5e41c
Samson Aderonmu <143148845+samsonHRC@users.noreply.github.com>
2024-09-02 23:10:35 +0100

    chore: Refactor AssetTable component to use filtered assets

--- client/src/components/AssetTable.tsx
+++ client/src/components/AssetTable.tsx
@@ -1,4 +1,3 @@
-import React from "react";
 import { Button } from "@/components/ui/button";
 import {
   Table,
@@ -13,56 +12,61 @@ import MemoAvax from "@/icons/Avax";
 import MemoCheckIcon from "@/icons/CheckIcon";
 import MemoXmarkIcon from "@/icons/XmarkIcon";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import { allAssets, Asset } from "@/lib/data";
 
 export default function AssetTable() {
+  const liquidAssets = allAssets.filter((asset) => asset.liquid);
+  const stakedAssets = allAssets.filter((asset) => asset.staked);
+  const savedAssets = allAssets.filter((asset) => asset.saved);
+
   return (
     <div className="bg-[#13131373] border border-[#13131373] p-4 rounded-[2rem] text-white ">
       <div className="mx-auto">
         <h1 className="text-xl font-semibold mb-4">Assets</h1>
 
         {/* Tabs Component */}
-        <Tabs defaultValue="all-assets" className="  w-full">
+        <Tabs defaultValue="all-assets" className="w-full">
           <TabsList className="flex space-x-4 bg-[#1E1E1E99] rounded-[2rem] p-2 mb-4">
             <TabsTrigger
               value="all-assets"
-              className=" text-white px-4 py-2 rounded-full">
+              className="text-white px-4 py-2 rounded-full">
               All assets
             </TabsTrigger>
             <TabsTrigger
               value="liquid-assets"
-              className=" text-white px-4 py-2 rounded-full">
+              className="text-white px-4 py-2 rounded-full">
               Liquid assets
             </TabsTrigger>
             <TabsTrigger
               value="staked-assets"
-              className=" text-white px-4 py-2 rounded-full">
+              className="text-white px-4 py-2 rounded-full">
               Staked assets
             </TabsTrigger>
             <TabsTrigger
               value="saved-assets"
-              className=" text-white px-4 py-2 rounded-full">
+              className="text-white px-4 py-2 rounded-full">
               Saved assets
             </TabsTrigger>
           </TabsList>
 
           {/* Tab Content for All Assets */}
           <TabsContent value="all-assets">
-            <AssetTableContent />
+            <AssetTableContent assets={allAssets} />
           </TabsContent>
 
           {/* Tab Content for Liquid Assets */}
           <TabsContent value="liquid-assets">
-            <AssetTableContent />
+            <AssetTableContent assets={liquidAssets} />
           </TabsContent>
 
           {/* Tab Content for Staked Assets */}
           <TabsContent value="staked-assets">
-            <AssetTableContent />
+            <AssetTableContent assets={stakedAssets} />
           </TabsContent>
 
           {/* Tab Content for Saved Assets */}
           <TabsContent value="saved-assets">
-            <AssetTableContent />
+            <AssetTableContent assets={savedAssets} />
           </TabsContent>
         </Tabs>
       </div>
@@ -70,7 +74,7 @@ export default function AssetTable() {
   );
 }
 
-function AssetTableContent() {
+function AssetTableContent({ assets }: { assets: Asset[] }) {
   return (
     <div className="bg-[#010104] p-4 rounded-lg">
       <CardContent>
@@ -80,52 +84,51 @@ function AssetTableContent() {
               <TableHead>Ticker</TableHead>
               <TableHead>Amount</TableHead>
               <TableHead className="hidden md:table-cell">Autosaved</TableHead>
-              <TableHead className="sr-only">Total Sales</TableHead>
-              <TableHead className="sr-only">Created at</TableHead>
               <TableHead>
                 <span className="sr-only">Actions</span>
               </TableHead>
             </TableRow>
           </TableHeader>
           <TableBody className="text-[#F1F1F1]">
-            {Array(5)
-              .fill(0)
-              .map((_, index) => (
-                <TableRow key={index}>
-                  <TableCell className="font-medium">
-                    <div className="flex items-center space-x-4">
-                      <MemoAvax className="w-6 h-6" />
-                      <div>
-                        <p className="font-[400] text-base">AVAX</p>
-                        <span className="font-[400] text-xs">Avalanche</span>
-                      </div>
+            {assets.map((asset, index) => (
+              <TableRow key={index}>
+                <TableCell className="font-medium">
+                  <div className="flex items-center space-x-4">
+                    <MemoAvax className="w-6 h-6" />
+                    <div>
+                      <p className="font-[400] text-base">{asset.symbol}</p>
+                      <span className="font-[400] text-xs">{asset.name}</span>
                     </div>
-                  </TableCell>
-                  <TableCell>
-                    <div className="flex flex-col">
-                      <p>0.00234 AVAX</p>
-                      <span className="text-xs">≈ $ 5.00</span>
-                    </div>
-                  </TableCell>
-                  <TableCell className="hidden md:table-cell">
-                    <div className="flex items-center">
-                      <span className="mr-2">Yes</span>
+                  </div>
+                </TableCell>
+                <TableCell>
+                  <div className="flex flex-col">
+                    <p>{asset.amount}</p>
+                    <span className="text-xs">{asset.value}</span>
+                  </div>
+                </TableCell>
+                <TableCell className="hidden md:table-cell">
+                  <div className="flex items-center">
+                    <span className="mr-2">
+                      {asset.autosaved ? "Yes" : "No"}
+                    </span>
+                    {asset.autosaved ? (
                       <MemoCheckIcon className="text-green-500 w-5 h-5" />
-                    </div>
-                  </TableCell>
-                  <TableCell className="flex">
-                    <Button variant="link" className="text-[#79E7BA]">
-                      Deposit
-                    </Button>
-                    <Button variant="link" className="text-[#79E7BA] ">
-                      Stake
-                    </Button>
-                    <Button variant="link" className="text-[#79E7BA] ">
-                      Save
-                    </Button>
-                  </TableCell>
-                </TableRow>
-              ))}
+                    ) : (
+                      <MemoXmarkIcon className="text-red-500 w-5 h-5" />
+                    )}
+                  </div>
+                </TableCell>
+                <TableCell className="flex">
+                  <Button variant="link" className="text-[#79E7BA]">
+                    Deposit
+                  </Button>
+                  <Button variant="link" className="text-[#79E7BA] ">
+                    Save
+                  </Button>
+                </TableCell>
+              </TableRow>
+            ))}
           </TableBody>
         </Table>
       </CardContent>
--- client/src/lib/data.ts
+++ client/src/lib/data.ts
@@ -4,6 +4,14 @@ import MemoReward from "@/icons/Reward";
 import MemoStaking from "@/icons/Staking";
 import MemoVault from "@/icons/Vault";
 
+export type Asset = {
+  symbol: string;
+  name: string;
+  amount: string;
+  value: string;
+  autosaved: boolean;
+};
+
 export const NavLinks = [
   {
     to: "/",
@@ -67,5 +75,68 @@ export const savings = [
         value: "$5.00",
       },
     ],
+  },
+];
+
+export const allAssets = [
+  {
+    symbol: "AVAX",
+    name: "Avalanche",
+    amount: "0.00234 AVAX",
+    value: "≈ $ 5.00",
+    autosaved: false,
+    staked: false,
+    liquid: false,
+    saved: true,
+  },
+  {
+    symbol: "AVAX",
+    name: "Avalanche",
+    amount: "0.00234 AVAX",
+    value: "≈ $ 5.00",
+    autosaved: true,
+    staked: true,
+    liquid: true,
+    saved: false,
+  },
+  {
+    symbol: "AVAX",
+    name: "Avalanche",
+    amount: "0.00234 AVAX",
+    value: "≈ $ 5.00",
+    autosaved: true,
+    staked: true,
+    liquid: false,
+    saved: false,
+  },
+  {
+    symbol: "AVAX",
+    name: "Avalanche",
+    amount: "0.00234 AVAX",
+    value: "≈ $ 5.00",
+    autosaved: true,
+    staked: false,
+    liquid: true,
+    saved: false,
+  },
+  {
+    symbol: "AVAX",
+    name: "Avalanche",
+    amount: "0.00234 AVAX",
+    value: "≈ $ 5.00",
+    autosaved: true,
+    staked: true,
+    liquid: false,
+    saved: false,
+  },
+  {
+    symbol: "AVAX",
+    name: "Avalanche",
+    amount: "0.00234 AVAX",
+    value: "≈ $ 5.00",
+    autosaved: true,
+    staked: false,
+    liquid: false,
+    saved: true,
   },
 ];
