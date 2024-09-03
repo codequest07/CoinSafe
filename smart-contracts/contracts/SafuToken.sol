616abd719c53b1cfe214cdfda92f1b0c04ab1461
Michael Nwachukwu <dahformulah@gmail.com>
2024-09-03 11:28:24 +0100

    Openzeppelin interfaces

--- smart-contracts/contracts/SafuToken.sol
+++ smart-contracts/contracts/SafuToken.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.19;
 
-// import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract Safu is ERC20("Safu Token", "SAFU") {
     address public owner;
