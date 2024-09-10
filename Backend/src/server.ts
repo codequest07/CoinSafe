d47a49a2e89253def934bd6e2af1ec952f15e715
Agbakwuru Oluchi <152165721+oluchicharity@users.noreply.github.com>
2024-09-10 21:46:23 +0100

    Oluchi's Backend

---
+++ Backend/src/server.ts
@@ -0,0 +1,17 @@
+import express, { Request, Response } from 'express';
+import cors from 'cors'; 
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(cors());
+
+app.get('/', (req: Request, res: Response) => {
+  res.send('Welcome to CoinSafe!');
+});
+
+//app.use('/api/v1');
+
+app.listen(port, () => {
+  console.log(`My Server is running on port ${port}.... keep off!`);
+});
