aa2db09b3ce38690f40b37700d89093b58a67a7a
Agbakwuru Oluchi <152165721+oluchicharity@users.noreply.github.com>
2024-09-18 22:15:13 +0100

    changes

--- Backend/src/server.ts
+++ Backend/src/server.ts
@@ -2,7 +2,7 @@ import express, { Request, Response } from 'express';
 import cors from 'cors'; 
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 1234;
 
 app.use(cors());
 
