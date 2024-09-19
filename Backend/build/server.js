4b0546398e446cb74d5b49f37fde16e67add7743
Agbakwuru Oluchi <152165721+oluchicharity@users.noreply.github.com>
2024-09-19 17:49:57 +0100

    database connection

--- Backend/build/server.js
+++ Backend/build/server.js
@@ -5,14 +5,13 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const cors_1 = __importDefault(require("cors"));
-const AiRouter_1 = __importDefault(require("./Routes/AiRouter"));
 const app = (0, express_1.default)();
 const port = process.env.PORT || 1234;
 app.use((0, cors_1.default)());
-app.get("/", (req, res) => {
-    res.send("Welcome to CoinSafe!");
+app.get('/', (req, res) => {
+    res.send('Welcome to CoinSafe!');
 });
-app.use("/api/v1", AiRouter_1.default);
+//app.use('/api/v1');
 app.listen(port, () => {
     console.log(`My Server is running on port ${port}.... keep off!`);
 });
