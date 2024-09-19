ea5119198e7fbe94ce1740383c965bd5495cd89b
Michael Nwachukwu <dahformulah@gmail.com>
2024-09-19 22:26:39 +0100

    updates on sc

--- smart-contracts/contracts/SafuToken.sol
+++ smart-contracts/contracts/SafuToken.sol
@@ -16,3 +16,6 @@ contract Safu is ERC20("Safu Token", "SAFU") {
         _mint(msg.sender, _amount * 1e18);
     }
 }
+
+// SafuModule#Safu - 0xBb88E6126FdcD4ae6b9e3038a2255D66645AEA7a
+// CoinsafeModule#Savings - 0x6330605C037437270aab6526263595c2297E4B5E
