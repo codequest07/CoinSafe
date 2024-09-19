6f1199a2ea44c553443fc17cdae39523bf9327b0
Agbakwuru Oluchi <152165721+oluchicharity@users.noreply.github.com>
2024-09-19 17:49:57 +0100

    database connection

--- Backend/build/server.js
+++ Backend/build/server.js
@@ -4,11 +4,14 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
+const cors_1 = __importDefault(require("cors"));
 const app = (0, express_1.default)();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 1234;
+app.use((0, cors_1.default)());
 app.get('/', (req, res) => {
     res.send('Welcome to CoinSafe!');
 });
+//app.use('/api/v1');
 app.listen(port, () => {
     console.log(`My Server is running on port ${port}.... keep off!`);
 });
