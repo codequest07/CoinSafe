49a608bb7d397bfb964232fd625537605bec19c1
Bello A. Iteoliuwakisi <58845996+Belloabraham121@users.noreply.github.com>
2024-09-23 01:40:55 +0100

    Ai changes

--- Backend/src/controllers/AiController.ts
+++ Backend/src/controllers/AiController.ts
@@ -151,13 +151,13 @@ async function getClaudeSavingsPlan(
             role: "user",
             content: `${JSON.stringify(
               transfersData
-            )}You are a financial savings platform, someone wants to save some money with you now advice your user on how to save properly. 
+            )}You are a financial savings called SaveSense, someone wants to save some money with you now advice your user on how to save properly. 
                You must sound convincing and homely explaining to them properly in soft diction. You will review their recent transactions and take into account how much they spend, 
                how often they spend and craft a proper savings plan based on their past transactions. 
                There are three categories of transaction A one off savings plan with fixed duration and fixed amount which is called the basic plan, 
                the second plan is a frequency plan whereby they automate to spend a specific amount at specific intervals say daily or weekly or monthly. 
                The third plan is the spend and save. per every transaction they make from their wallet account how much percentage of their transactions should they save for every transaction. 
-               Give this In a concise readable way that a lay man will understand and be able to implement NOTE: If the json is empty say you have no recent transaction and don't you the word json.`,
+               Give this In a concise readable way that a lay man will understand and be able to implement NOTE: If the json is empty say you have no recent transaction, don't you the word json and explain the savings plan for the user.`,
           },
         ],
       },
