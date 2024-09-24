5ad693550916094a6da9fde0015dcf101b702260
AMD <michojekunle1@gmail.com>
2024-09-24 16:54:43 +0100

    feat: dashboard err fixed for displaying balances

--- client/src/lib/utils.ts
+++ client/src/lib/utils.ts
@@ -4,3 +4,7 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+export function getValidNumberValue(value: any) {
+  return typeof value === "number" && !isNaN(value) ? value : 0;
+}
