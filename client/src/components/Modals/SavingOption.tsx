4a424f807558d5202f6df77a39ba9bc6209a7150
AMD <michojekunle1@gmail.com>
2024-12-07 09:30:46 +0100

    personalised AI working

--- client/src/components/Modals/SavingOption.tsx
+++ client/src/components/Modals/SavingOption.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useRef } from "react";
 import {
   Dialog,
   DialogContent,
@@ -12,6 +12,7 @@ import { Label } from "@/components/ui/label";
 import SaveAsset from "./SaveAsset";
 import { useRecoilState } from "recoil";
 import { saveAtom } from "@/store/atoms/save";
+import { SaveSenseModalManager } from "../Cards/SaveSenseModalManager";
 
 interface SavingOptionProps {
   isFirstModalOpen: boolean;
@@ -26,12 +27,27 @@ export default function SavingOption({
   isSecondModalOpen,
   setIsSecondModalOpen,
 }: SavingOptionProps) {
+  const [saveState, setSaveState] = useRecoilState(saveAtom);
+
+  const modalManagerRef = useRef<{ 
+    fetchData: () => void; 
+    download: () => void; 
+  }>(null);
+
+  const handleButtonClick = () => {  
+      // Trigger fetch data method on modal manager
+      modalManagerRef.current?.fetchData();
+  };
+
   const openSecondModal = () => {
-    setIsFirstModalOpen(false);
+    if(saveState.typeName === "personalized") {
+      setIsFirstModalOpen(false);
+      handleButtonClick();
+      return 
+    }
     setIsSecondModalOpen(true);
   };
 
-  const [_, setSaveState] = useRecoilState(saveAtom);
 
   const handleChange = (event:any) => {
     setSaveState((prevState) => ({...prevState, typeName: event.target.value}));
@@ -112,6 +128,13 @@ export default function SavingOption({
           setIsFirstModalOpen(true);
         }}
         tab=""
+      />
+       {/* Modal Manager Component */}
+       <SaveSenseModalManager 
+        trigger={modalManagerRef}
+        onClose={() => {
+          // Optional: Add any cleanup or additional logic when modals close
+        }} 
       />
     </Dialog>
   );
