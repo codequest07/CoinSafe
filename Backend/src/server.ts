2439bca37b7c17b17540c47a2f710be92b5b4c69
Agbakwuru Oluchi <152165721+oluchicharity@users.noreply.github.com>
2024-12-08 14:55:54 +0100

    added-waitlist-functionality

--- Backend/src/server.ts
+++ Backend/src/server.ts
@@ -3,9 +3,13 @@ import cors from "cors";
 import AiRouter from "./Routes/AiRouter";
 import CoinGeckoApiRouter from "./Routes/CoinGeckoApiRouter";
 import BaseRouter from "./Routes/BaseRouter";
-
-
+import mongoose from "mongoose";
+import { ConnectOptions } from "mongoose";
+import WaitlistRouter from "./Routes/WaitlistRouter";
+import dotenv from "dotenv"
+dotenv.config()
 const app = express();
+app.use(express.json());
 const port = process.env.PORT || 1234;
 
 app.use(cors());
@@ -18,8 +22,18 @@ app.use(AiRouter);
 
 app.use('/', BaseRouter)
 
-app.use(CoinGeckoApiRouter);
+app.use('/',WaitlistRouter);
 
+app.use(CoinGeckoApiRouter);
+const mongodb = process.env.MONGO_URI || 'mongodb+srv://agbakwuruoluchicoinsafe:SDYRnmD6FrVp09fo@cluster0.g6csr.mongodb.net';
+mongoose.connect(mongodb, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+} as ConnectOptions).then(() => {
+  console.log('Connected to MongoDB');
+}).catch((error) => {
+  console.error('Error connecting to MongoDB:', error.message);
+});
 app.listen(port, () => {
   console.log(`My Server is running on port ${port}.... keep off!`);
 });
