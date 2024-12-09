b1eba614d770927d842131960c7a9d46a46cb782
Superior <samsonaderonmu2021@gmail.com>
2024-12-09 03:36:49 +0100

    Add FeatureCards component and update LandingPage layout

--- client/src/components/TrackProgress.tsx
+++ client/src/components/TrackProgress.tsx
@@ -14,17 +14,23 @@ const TrackProgress = () => {
   return (
     <div className="relative flex justify-center px-4 sm:px-8 md:px-16 lg:px-24">
       <div className="w-full max-w-screen-lg">
-        <div className="flex flex-col md:flex-row justify-between items-center pt-12 md:pt-[200px] pb-8 md:pb-[80px]">
-          <div className="text-[24px] md:text-[40px] text-[#F1F1F1] max-w-full md:max-w-[658px] text-center md:text-left">
+        <div className="flex flex-col md:flex-row justify-center items-center pt-12 md:pt-[200px] pb-8 md:pb-[80px]">
+          <div
+            style={{
+              background: "linear-gradient(to right, #F1F1F1, #8B8B8B)",
+              WebkitBackgroundClip: "text",
+              WebkitTextFillColor: "transparent",
+            }}
+            className="text-[24px] md:text-[40px] text-[#F1F1F1] max-w-full md:max-w-[588px] text-center">
             Track your progress, manage your savings, and stay in control
           </div>
-          <div className="mt-6 md:mt-0">
+          {/* <div className="mt-6 md:mt-0">
             <button
               onClick={() => navigate("/dashboard")}
               className="bg-[#1E1E1E99] text-[#F1F1F1] px-4 py-2 md:px-6 md:py-3 rounded-full">
               Get Started
             </button>
-          </div>
+          </div> */}
         </div>
         <div data-aos="fade-up" className="w-full flex justify-center">
           <img
