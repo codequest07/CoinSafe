38099b8ade1b976ad1faf8527a63b0ce93f8883c
Levi Francis <levifrancis15@gmail.com>
2024-12-09 12:38:05 +0100

    add eslint rule

--- client/eslint.config.js
+++ client/eslint.config.js
@@ -24,6 +24,8 @@ export default tseslint.config(
         { allowConstantExport: true },
       ],
       "no-unused-vars": ["error", { "varsIgnorePattern": "React" }],
+      "@typescript-eslint/no-explicit-any": "off",
+      "@typescript-eslint/no-unused-vars": "off"
     },
   },
 )
