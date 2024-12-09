03252cd61e89620490e6e0573a2db4ceb750081d
Agbakwuru Oluchi <152165721+oluchicharity@users.noreply.github.com>
2024-12-09 12:59:54 +0100

    email functionality

--- Backend/build/Routes/WaitlistRouter.js
+++ Backend/build/Routes/WaitlistRouter.js
@@ -5,5 +5,6 @@ const WaitlistController_1 = require("../controllers/WaitlistController");
 const WaitlistRouter = (0, express_1.Router)();
 WaitlistRouter.post('/api/waitlist', WaitlistController_1.addToWaitlist);
 WaitlistRouter.get('/api/waitlist', WaitlistController_1.getAllWaitlistEntries);
+WaitlistRouter.get('/waitlist/:email', WaitlistController_1.getOne);
 exports.default = WaitlistRouter;
 //# sourceMappingURL=WaitlistRouter.js.map
--- Backend/src/Routes/WaitlistRouter.ts
+++ Backend/src/Routes/WaitlistRouter.ts
@@ -7,6 +7,6 @@ WaitlistRouter.post('/api/waitlist', addToWaitlist);
 
 WaitlistRouter.get('/api/waitlist', getAllWaitlistEntries);
 
-WaitlistRouter.get('/waitlist/:email', getOne);
+WaitlistRouter.get('/api/waitlist/:email', getOne);
 
 export default WaitlistRouter;
