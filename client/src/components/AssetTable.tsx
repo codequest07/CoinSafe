f44bf9ead74b758e696a879ea24a7e554a3ab122
AMD <michojekunle1@gmail.com>
2024-12-13 14:13:24 +0100

    edits

--- client/src/components/AssetTable.tsx
+++ client/src/components/AssetTable.tsx
@@ -17,6 +17,8 @@ import { CoinSafeContract } from "@/lib/contract";
 import { useEffect, useState } from "react";
 import SavingOption from "./Modals/SavingOption";
 import { transformAndAccumulateTokenBalances } from "@/lib/utils";
+import Deposit from "./Modals/Deposit";
+import MemoMoney from "@/icons/Money";
 
 export default function AssetTable() {
   const [allAssetData, setAllAssetData] = useState([]);
@@ -85,14 +87,15 @@ export default function AssetTable() {
 
     if (liquidAssets.length === 0) return;
     // Map through the 2D array to create objects
-    const liquidAssetsRes = liquidAssets[0]?.map(
-      (key: any, index: string | number) => {
+    const liquidAssetsRes = liquidAssets[0]
+      ?.map((key: any, index: string | number) => {
+        if (Number(liquidAssets[1][index]) <= 0) return;
         return {
           token: key,
           balance: formatEther(liquidAssets[1][index]),
         };
-      }
-    );
+      })
+      .filter((entry: any) => entry !== undefined);
     setLiquidAssetData(liquidAssetsRes);
 
     if (savedAssets.length === 0) return;
@@ -104,6 +107,7 @@ export default function AssetTable() {
     console.log("All Token Balances", AllTokenBalances);
     console.log("Liquid Token Balances", LiquidTokenBalances);
     console.log("Saved Token Balances", SavedTokenBalances);
+    console.log("Liquid Assets", liquidAssetsRes);
   }, [allAssets, liquidAssets, savedAssets]);
 
   return (
@@ -114,17 +118,20 @@ export default function AssetTable() {
           <TabsList className="sm:flex space-x-4 hidden bg-[#1E1E1E99] rounded-[2rem] p-2 mb-4">
             <TabsTrigger
               value="all-assets"
-              className="text-white px-4 py-2 rounded-full">
+              className="text-white px-4 py-2 rounded-full"
+            >
               All assets
             </TabsTrigger>
             <TabsTrigger
               value="liquid-assets"
-              className="text-white px-4 py-2 rounded-full">
+              className="text-white px-4 py-2 rounded-full"
+            >
               Liquid assets
             </TabsTrigger>
             <TabsTrigger
               value="saved-assets"
-              className="text-white px-4 py-2 rounded-full">
+              className="text-white px-4 py-2 rounded-full"
+            >
               Saved assets
             </TabsTrigger>
           </TabsList>
@@ -175,7 +182,8 @@ function AssetTableContent({ assets }: { assets: any[] }) {
           </h3>
           <Button
             onClick={openDepositModal}
-            className="mt-4 bg-[#1E1E1E99] rounded-[2rem] text-[#F1F1F1] hover:bg-[#2a2a2a]">
+            className="mt-4 bg-[#1E1E1E99] rounded-[2rem] text-[#F1F1F1] hover:bg-[#2a2a2a]"
+          >
             Deposit
           </Button>
         </div>
@@ -208,7 +216,8 @@ function AssetTableContent({ assets }: { assets: any[] }) {
             {assets.map((asset, index) => (
               <TableRow
                 key={index}
-                className="w-full flex flex-col sm:table-row">
+                className="w-full flex flex-col sm:table-row"
+              >
                 <TableCell className="w-full sm:w-1/3">
                   <div className="flex items-center space-x-4">
                     <div className="flex flex-col">
@@ -241,13 +250,15 @@ function AssetTableContent({ assets }: { assets: any[] }) {
                   <Button
                     variant="link"
                     className="text-[#79E7BA]"
-                    onClick={() => setIsDepositModalOpen(true)}>
+                    onClick={() => setIsDepositModalOpen(true)}
+                  >
                     Deposit
                   </Button>
                   <Button
                     variant="link"
                     className="text-[#79E7BA]"
-                    onClick={() => setIsFirstModalOpen(true)}>
+                    onClick={() => setIsFirstModalOpen(true)}
+                  >
                     Save
                   </Button>
                 </TableCell>
@@ -261,6 +272,11 @@ function AssetTableContent({ assets }: { assets: any[] }) {
         setIsFirstModalOpen={setIsFirstModalOpen}
         isSecondModalOpen={isSecondModalOpen}
         setIsSecondModalOpen={setIsSecondModalOpen}
+      />
+      <Deposit
+        isDepositModalOpen={isDepositModalOpen}
+        setIsDepositModalOpen={setIsDepositModalOpen}
+        onBack={() => {}}
       />
     </div>
   );
