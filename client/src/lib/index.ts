4a5c2d53623179d1442d0afda8dd6b012647522c
AMD <michojekunle1@gmail.com>
2025-01-01 18:06:39 +0100

    feat: implement caching and rate limiting for CoinGecko API requests

--- client/src/lib/index.ts
+++ client/src/lib/index.ts
@@ -1,16 +1,12 @@
 // export const base_uri_test = import.meta.env.DEV ? 'http://localhost:1234' : 'https://coinsafe-1-1jw5.onrender.com';
-export const base_uri = 'https://coinsafe-1-1jw5.onrender.com';
+export const base_uri = "https://coinsafe-1-1jw5.onrender.com";
 
 export const getLskToUsd = async (lsk: number) => {
   try {
     const res = await fetch(`${base_uri}/api-cg/lisk`);
-    console.log(res);
     const data = await res.json();
 
-    // console.log(data?.lisk?.usd);
-
     if (data?.lisk?.usd) {
-        // console.log("return val: ", data.lisk.usd * lsk)
       return data.lisk.usd * lsk;
     } else {
       throw new Error("LSK data or USD price not available");
@@ -30,11 +26,7 @@ export const getUsdtToUsd = async (usdt: number) => {
     const res = await fetch(`${base_uri}/api-cg/tether`);
     const data = await res.json();
 
-    // console.log(data?.tether?.usd);
-
     if (data?.tether?.usd) {
-        // console.log("return val: ", data.tether.usd * usdt)
-
       return data.tether.usd * usdt;
     } else {
       throw new Error("USDT data or USD price not available");
