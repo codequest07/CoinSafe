53d2277e0554d22c2ff1f2a8a1e119414cb290a2
Levi Francis <levifrancis15@gmail.com>
2025-04-04 00:31:25 +0100

    create save assets page and savings target

---
+++ client/src/Pages/SaveAssets.tsx
@@ -0,0 +1,12 @@
+import SaveAssetsCard from "@/components/Cards/SaveAssetsCard";
+// import React from "react";
+
+const SaveAssets = () => {
+  return (
+    <div className="bg-[#010104]">
+      <SaveAssetsCard />
+    </div>
+  );
+};
+
+export default SaveAssets;
