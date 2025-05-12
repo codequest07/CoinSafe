e2866d9ba5885b2c61c81efbcd6174895b9ff359
Levi <a1234@Levis-MacBook-Pro-2.local>
2025-05-12 22:02:06 +0100

    change auto safe amount saved

--- client/src/components/SavingsCards.tsx
+++ client/src/components/SavingsCards.tsx
@@ -124,7 +124,8 @@ export default function SavingsCards() {
               {[1, 2, 3].map((i) => (
                 <div
                   key={i}
-                  className="shrink-0 w-[280px] p-6 rounded-lg border border-[#FFFFFF21]">
+                  className="shrink-0 w-[280px] p-6 rounded-lg border border-[#FFFFFF21]"
+                >
                   <div className="flex justify-between items-start mb-4">
                     <Skeleton className="h-4 w-24" />
                     <Skeleton className="h-6 w-16 rounded-xl" />
@@ -149,7 +150,8 @@ export default function SavingsCards() {
               {[1, 2, 3].map((i) => (
                 <div
                   key={i}
-                  className="shrink-0 w-[280px] p-6 rounded-lg border border-[#FFFFFF21]">
+                  className="shrink-0 w-[280px] p-6 rounded-lg border border-[#FFFFFF21]"
+                >
                   <div className="flex justify-between items-start mb-4">
                     <Skeleton className="h-4 w-24" />
                     <Skeleton className="h-6 w-16 rounded-xl" />
@@ -166,10 +168,12 @@ export default function SavingsCards() {
               <div
                 ref={scrollContainerRef}
                 className="flex space-x-4 pb-4 overflow-x-auto hide-scrollbar"
-                style={{ scrollbarWidth: "none", msOverflowStyle: "none" }}>
+                style={{ scrollbarWidth: "none", msOverflowStyle: "none" }}
+              >
                 <button
                   onClick={() => navigate("/dashboard/vault/auto-safe")}
-                  className="text-left shrink-0 w-[280px] p-6 rounded-lg border border-[#FFFFFF21] transition-colors">
+                  className="text-left shrink-0 w-[280px] p-6 rounded-lg border border-[#FFFFFF21] transition-colors"
+                >
                   <div className="flex justify-between items-start mb-4">
                     <div className="text-sm text-gray-400 font-[300]">
                       {"Auto-savings"}
@@ -177,7 +181,8 @@ export default function SavingsCards() {
                     <Badge
                       className={`
                         bg-[#79E7BA33] font-[400] text-[#F1F1F1] rounded-xl flex items-center p-1 px-2 hover:bg-[#79E7BA33]
-                      `}>
+                      `}
+                    >
                       {"Locked"}
                     </Badge>
                   </div>
@@ -193,7 +198,7 @@ export default function SavingsCards() {
                       {Number(
                         formatUnits(
                           details?.tokenDetails?.reduce(
-                            (total: any, obj: any) => total + obj.amountToSave,
+                            (total: any, obj: any) => total + obj.amountSaved,
                             0n
                           ),
                           18
@@ -219,7 +224,8 @@ export default function SavingsCards() {
                           : `/dashboard/vault/${safe.id}`
                       )
                     }
-                    className="text-left shrink-0 w-[280px] p-6 rounded-lg border border-[#FFFFFF21] transition-colors">
+                    className="text-left shrink-0 w-[280px] p-6 rounded-lg border border-[#FFFFFF21] transition-colors"
+                  >
                     <div className="flex justify-between items-start mb-4">
                       <div className="text-sm text-gray-400 font-[300]">
                         {safe.name}
@@ -227,7 +233,8 @@ export default function SavingsCards() {
                       <Badge
                         className={`
                         bg-[#79E7BA33] font-[400] text-[#F1F1F1] rounded-xl flex items-center p-1 px-2 hover:bg-[#79E7BA33]
-                      `}>
+                      `}
+                      >
                         {safe.status}
                       </Badge>
                     </div>
@@ -253,7 +260,8 @@ export default function SavingsCards() {
             variant="ghost"
             size="icon"
             onClick={handleScrollBack}
-            className="absolute left-0 top-1/2 -translate-y-1/2 h-8 w-8 rounded-full bg-black/80 hover:bg-black/70 text-white z-10 border border-gray-700">
+            className="absolute left-0 top-1/2 -translate-y-1/2 h-8 w-8 rounded-full bg-black/80 hover:bg-black/70 text-white z-10 border border-gray-700"
+          >
             <ChevronLeft className="h-4 w-4" />
           </Button>
 
@@ -261,7 +269,8 @@ export default function SavingsCards() {
             variant="ghost"
             size="icon"
             onClick={handleScroll}
-            className="absolute right-0 top-1/2 -translate-y-1/2 h-8 w-8 rounded-full bg-black/80 hover:bg-black/70 text-white z-10 border border-gray-700">
+            className="absolute right-0 top-1/2 -translate-y-1/2 h-8 w-8 rounded-full bg-black/80 hover:bg-black/70 text-white z-10 border border-gray-700"
+          >
             <ChevronRight className="h-4 w-4" />
           </Button>
         </div>
