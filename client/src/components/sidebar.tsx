117ea528bb7b84e3f1f6f71bb17d7eeeb2f030d4
Samson <samsonaderonmu2021@gmail.com>
2025-07-29 07:34:27 +0100

    refactor: Update Sidebar layout for improved structure and responsiveness
    - Changed the main container to a grid layout for better organization.
    - Adjusted the visibility of the sidebar for mobile and desktop views.
    - Maintained existing navigation functionality while enhancing the overall design.

--- client/src/components/sidebar.tsx
+++ client/src/components/sidebar.tsx
@@ -43,43 +43,45 @@ const Sidebar = () => {
   };
 
   return (
-    <div className="hidden md:block">
-      <div className="rounded-2xl border-r p-2 border-[#13131373] bg-[#13131373] flex flex-col h-full">
-        <div className="flex h-full max-h-fit shadow-lg rounded-xl flex-col gap-2">
-          <div className="flex items-center py-12 px-4 lg:h-[60px] lg:px-6">
-            <Link
-              to="/dashboard"
-              className="flex items-center gap-2 font-semibold">
-              <MemoLogo className="w-40 h-40" />
-            </Link>
+    <main>
+      <div className="grid min-h-screen w-full md:grid-cols-[240px_1fr] lg:grid-cols-[280px_1fr]">
+        <div className="hidden rounded-2xl border-r p-2 border-[#13131373] bg-[#13131373] md:flex flex-col">
+          <div className="flex h-full max-h-fit shadow-lg rounded-xl flex-col gap-2">
+            <div className="flex items-center py-12 px-4 lg:h-[60px] lg:px-6">
+              <Link
+                to="/dashboard"
+                className="flex items-center gap-2 font-semibold">
+                <MemoLogo className="w-40 h-40" />
+              </Link>
+            </div>
+            <div className="flex-1">
+              <nav className="grid items-start px-2 text-sm font-medium lg:px-4">
+                {NavLinks.map((link) => (
+                  <NavLink
+                    key={link.label}
+                    to={link.to}
+                    onClick={handleNavigate}
+                    className={() =>
+                      isLinkActive(link.to)
+                        ? "flex items-center gap-3 font-[400] rounded-lg px-3 py-3 my-1.5 text-[#F1F1F1] bg-[#1E1E1E99] transition-all"
+                        : "flex items-center gap-3 font-[400] rounded-lg px-3 py-3 my-1.5 text-[#B5B5B5] transition-all"
+                    }>
+                    <>
+                      {isLinkActive(link.to) ? (
+                        <link.activeIcon className="w-5 h-5" />
+                      ) : (
+                        <link.icon className="w-5 h-5" />
+                      )}
+                      {link.label}
+                    </>
+                  </NavLink>
+                ))}
+              </nav>
+            </div>
           </div>
-          <div className="flex-1">
-            <nav className="grid items-start px-2 text-sm font-medium lg:px-4">
-              {NavLinks.map((link) => (
-                <NavLink
-                  key={link.label}
-                  to={link.to}
-                  onClick={handleNavigate}
-                  className={() =>
-                    isLinkActive(link.to)
-                      ? "flex items-center gap-3 font-[400] rounded-lg px-3 py-3 my-1.5 text-[#F1F1F1] bg-[#1E1E1E99] transition-all"
-                      : "flex items-center gap-3 font-[400] rounded-lg px-3 py-3 my-1.5 text-[#B5B5B5] transition-all"
-                  }>
-                  <>
-                    {isLinkActive(link.to) ? (
-                      <link.activeIcon className="w-5 h-5" />
-                    ) : (
-                      <link.icon className="w-5 h-5" />
-                    )}
-                    {link.label}
-                  </>
-                </NavLink>
-              ))}
-            </nav>
+          <div className="mt-auto p-4">
+            <ExtensionCard />
           </div>
-        </div>
-        <div className="mt-auto p-4">
-          <ExtensionCard />
         </div>
       </div>
       {openConnectModal && (
@@ -88,7 +90,7 @@ const Sidebar = () => {
           setIsConnectModalOpen={setOpenConnectModal}
         />
       )}
-    </div>
+    </main>
   );
 };
 
