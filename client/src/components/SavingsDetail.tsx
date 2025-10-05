d781d73df3950bc92c6b75eca04490aacb19c4c8
AMD <michojekunle1@gmail.com>
2025-10-05 05:06:52 +0100

    fix: update unlock status message from "Ready to unlock" to "Matured" for matured safes

--- client/src/components/SavingsDetail.tsx
+++ client/src/components/SavingsDetail.tsx
@@ -191,7 +191,7 @@ export default function SavingsDetail() {
                               new Date().getTime()) /
                               (1000 * 60 * 60 * 24)
                           )} days till unlock`
-                        : "Ready to unlock"
+                        : "Matured"
                       : "Flexible"}
                   </Badge>
                 </div>
