6d54b35eb2d362b4f4f8b35b95752472234bc50c
Samson Aderonmu <Superior212@users.noreply.github.com>
2026-02-23 13:01:51 +0100

    feat: Add isEmergencyPage prop to TargetAssetTable and clean up button click handlers

--- client/src/lib/contract.ts
+++ client/src/lib/contract.ts
@@ -1,10 +1,10 @@
 import faucetAbi from "../abi/faucet.json";
-import coinSafeAbi from "../abi/coinsafe.json";
 import balanceFacetAbi from "../abi/BalanceFacet.json";
 import targetSavingsAbi from "../abi/TargetSavingsFacet.json";
 import automatedSavingsFacetAbi from "../abi/AutomatedSavingsFacet.json";
 import emergencySavingsFacetAbi from "../abi/EmergencySavingsFacet.json";
 import fundingFacetAbi from "../abi/FundingFacet.json";
+import swapFacetAbi from "../abi/SwapFacet.json";
 
 /**
  * @deprecated Use useChainConfig() hook to access chain-specific tokens.
@@ -12,22 +12,17 @@ import fundingFacetAbi from "../abi/FundingFacet.json";
  */
 export const tokens = {
   usdt: "0x05D032ac25d322df992303dCa074EE7392C117b9",
+  usdt0: "0x43F2376D5D03553aE72F4A8093bbe9de4336EB08",
   usdc: "0xF242275d3a6527d877f2c927a82D9b057609cc71",
   safu: "0xBb88E6126FdcD4ae6b9e3038a2255D66645AEA7a",
   lsk: "0xac485391EB2d7D88253a7F1eF18C37f4242D1A24",
-
-};
-
-export const CoinSafeContract = {
-  address: "0x727b742EAd5540703b62C7f33312335dcFd83f5A",
-  abi: coinSafeAbi,
 };
 
 // 0x08b59fb7ee9418470004693acf6d2c7dd0256a69
 // unkonw 0x69B417f1937EF6fF93471a247D70a245B6216ca8
 export const CoinsafeDiamondContract = {
   //mainnet address
-  address: "0xAb82eA18aA9b0F74DEc7F2e7bAdF9D47eF380ADB",
+  address: "0x1f9d3d871f5D279e939B0622eCEb130aB35c8FF2",
   // address: "0x08b59fb7ee9418470004693acf6d2c7dd0256a69",
   // old diamond address
   //address: "0xda6cf4d86ac271686ace56c554acad7bc6940667",
@@ -44,4 +39,5 @@ export const facetAbis = {
   automatedSavingsFacet: automatedSavingsFacetAbi,
   targetSavingsFacet: targetSavingsAbi,
   emergencySavingsFacet: emergencySavingsFacetAbi,
+  swapFacet: swapFacetAbi,
 };
